592f95aac3ebab30ea15e210f1e3bdad6e1a11d7
sabotack <sabotack.2001@gmail.com>
2022-06-30 17:39:34 +0200

    Padding & animation changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
-    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.querySelector('header').classList.add("header-scroll");
     }
     else {
