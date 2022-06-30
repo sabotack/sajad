c21ec3bdd9a96bf64beb9f149011ff7ada34de45
sabotack <sabotack.2001@gmail.com>
2022-06-30 19:23:41 +0200

    Added mobile navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,28 @@
-window.onscroll = function() {
+let qsNav = document.querySelector('nav');
+let qsNavModalBg = document.querySelector('.nav-modal-bg');
+let qsBurgerMenu = document.querySelector('#burger-menu');
+
+
+qsBurgerMenu.addEventListener('click', () => {
+    qsBurgerMenu.children[0].classList.toggle('active');
+    
+    if (qsNav.style.display != 'flex') {
+        qsNav.style.display = 'flex';
+        qsNavModalBg.style.display = 'block';
+    }
+    else {
+        qsNav.style = '';
+        qsNavModalBg.style = '';
+    }
+});
+
+qsNavModalBg.addEventListener('click', () => {
+    qsNav.style = '';
+    qsNavModalBg.style = '';
+    qsBurgerMenu.children[0].classList.remove('active');
+});
+
+window.onscroll = () => {
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.querySelector('header').classList.add("header-scroll");
     }
