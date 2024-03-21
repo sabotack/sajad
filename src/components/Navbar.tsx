b9694f6e8a1444e31e4a43f51b2ca9c8a64ad7b9
sabotack <sabotack.2001@gmail.com>
2024-03-21 14:59:39 +0100

    Fix logo not included in build folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,13 @@
 import { NavLink } from 'react-router-dom';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 import { SiPolymerproject } from 'react-icons/si';
-import Logo from '../assets/logo_circle.svg'
+import Logo from '../assets/logo.webp'
 
 function Navbar() {
     return (
         <header className="fixed w-full h-header px-32 bg-primary flex items-center justify-between z-20">
             <div className='flex items-center'>
-                <div className="w-10 h-10 mr-4 bg-no-repeat bg-cover" style={{background: `url(${Logo})`}} />
+                <div className="w-10 h-10 mr-4 bg-no-repeat bg-cover bg-center" style={{backgroundImage: `url(${Logo})`}} />
                 <div className="text-2xl font-bold text-white">ALI SAJAD KHORAMI</div>
             </div>
             <nav className="flex">
