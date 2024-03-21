fe1071eb65d2549497dc8caf284d3b5f6c368e14
sabotack <sabotack.2001@gmail.com>
2024-03-21 20:53:53 +0100

    Initial vite svelte tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'primary': {
-          DEFAULT: 'var(--primary)',
-          'mid': 'var(--primary-mid)',
-          'light': 'var(--primary-light)',
-        },
-        'accent': 'var(--accent)',
-      },
-      backgroundImage: {
-        'gradient-home-r': 'var(--gradient-home-r)',
-        'gradient-home-b': 'var(--gradient-home-b)',
-      },
-      spacing: {
-        'header': 'var(--header-height)',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
